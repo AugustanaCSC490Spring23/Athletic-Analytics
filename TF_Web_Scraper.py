515f0b4feeb0ca8fa004f177a9f4db0c15770c2c
brianholton19 <71453624+brianholton19@users.noreply.github.com>
2023-04-25 10:45:19 -0500

    Add files via upload
    Newest version

--- TF_Web_Scraper.py
+++ TF_Web_Scraper.py
@@ -9,13 +9,19 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-oConferenceList = [4268, 4248, 4237, 4270, 4246, 4301, 4267, 4242, 4245, 4244, 4273,
-                    4239, 4241, 4300, 4247, 4272, 4243, 4249, 4250, 4251, 4252, 4254,
-                    4238, 4253, 4274, 4258, 4294, 4240, 4255, 4256, 4269, 4257, 4222,
-                    4260, 4259, 4261, 4235, 4234, 4265, 4262, 4236, 4263, 4264, 4271,
-                    4266]
+diii = [4268, 4248, 4237, 4270, 4246, 4301, 4267, 4242, 4245, 4244, 4273,
+        4247, 4272, 4243, 4249, 4250, 4251, 4252, 4254, 4238, 4253, 4274,
+        4258, 4294, 4240, 4255, 4256, 4269, 4257, 4222, 4260, 4259,
+        4261, 4235, 4234, 4265, 4262, 4236, 4263, 4264, 4271, 4266]
+# dii = [4200,4191,4180,4181,4194,4182,4183,4184,4199,4196,4198,
+#        4189,4185,4195,4186,4187,4179,4192,4193,4188,4190,4197]
+# diii = [4268]
+
+# di = [4280,4277,4279,4278,4296,4231,4281,4282,4229,4228,4224,4230,
+#       4283,4295,4276,4285,4289,4299,4284,4291,4233,4287,4294,
+#       4292,4232,4225,4286,4223,4290,4275,4227,4226,4293,4288]
 o = 0
-for i in oConferenceList:
+for i in diii:
     name = 'output' + str(i) + '.csv'
     urlName = 'https://www.tfrrs.org/lists/' + str(i) + '/'
     url = urlName
@@ -29,6 +35,14 @@ for i in oConferenceList:
     events = [event.text.strip() for event in events]
     conference = events[0]
     
+    # splitting off outdoor performance list
+    con = conference.split()
+    if len(con)==7:
+        conference = "CCS"
+        con = ""
+    else:
+        conference = " ".join(con[0:-3])
+    
     # Getting the headers for the columns
     # o==0 is just so that that
     # section of code runs only once
@@ -37,8 +51,16 @@ for i in oConferenceList:
             he = head.find_all('th')
             he=[h.text.strip() for h in he]
             he[0]='Rank'
-            he[1] = 'Athlete(s)'
-            he[4]='Time/Distance/Points'
+            he[1] = 'Athlete'
+            he[4]='Time'
+            meet = he[5]
+            he[5] = 'Distance'
+            date = he[6]
+            he[6]='Points'
+            wind = he[7]
+            he[7]=meet
+            he.append(date)
+            he.append(wind)
             he.append('Event')
             he.append('Conference')
             he.append('Gender')
@@ -55,7 +77,17 @@ for i in oConferenceList:
                 # only has to run once per event
                 l=0
                 k=k+1
+                if l == 0:
+                    event = events[k].split()
+                    gender=event[-1]
+                    event = " ".join(event[0:len(event)-1])
+                    if gender == '(Men)':
+                        gender = 'Men'
+                    elif gender == '(Women)':
+                        gender = 'Women'
+                    l = 1
             if cols!=[]:
+                
                 # these two if statements are to rearrange
                 # the columns during multi-people events
                 if cols[3] not in colleges:
@@ -67,44 +99,122 @@ for i in oConferenceList:
                     time=temp[2]
                     temp[2]="NA"
                     temp[3]=college
-                    temp.append(temp[5])
-                    temp[5]=temp[4]
+                    meet=temp[4]
+                    date=temp[5]
                     temp[4]=time
+                    temp[5]=""
+                    temp.append("")
+                    temp.append(meet)
+                    temp.append(date)
+                    temp.append("")
                     cols=temp
-
+                    split = cols[4].split(":")
+                    if len(split)>1:
+                        minute=split[0]
+                        sec = split[1].split("\n")
+                        sec = round(float(sec[0]))
+                        if sec<60:
+                            time = str(minute) + ":"+str(sec)
+                        else:
+                            minute=int(minute)+1
+                            sec=":00"
+                            time=str(minute) + sec
+                        cols[4]=time
+                
+                elif k<=22:
+                    split = cols[4].split(":")
+                    if len(split)>1:
+                        minute=split[0]
+                        sec = split[1].split("\n")
+                        sec = round(float(sec[0]))
+                        #sec = round(float(split[1].strip().replace("\n","")))
+                        if sec<60:
+                            time = str(minute) + ":"+str(sec)
+                        else:
+                            minute=int(minute)+1
+                            sec=":00"
+                            time=str(minute) + sec
+                        cols[4]=time
+                    meet=cols[5]
+                    date=cols[6]
+                    if len(cols)==7:
+                        wind=""
+                        cols.append(meet)
+                    elif cols[7] != "":
+                        wind=cols[7]
+                        if cols[7]=="NWI":
+                            wind=""
+                        cols[7]=meet
+                    cols[5]=""
+                    cols[6]=""
+                    
+                    cols.append(date)
+                    cols.append(wind)
                 # checking for the distance events
-                elif len(cols)>8 or (k>22):
+                elif k>22:
                     split = cols[4].split('.')
                     if len(split)>1:
-                        temp2=cols
-                        temp2[5]=temp2[6]
-                        temp2[6]=temp2[7]
-                        if len(cols)!= 9:
-                            temp2[7]=""
+                        if event=="Long Jump" or event == "Triple Jump":
+                            temp2=cols
+                            distance = temp2[4]
+                            meet=temp2[6]
+                            date=temp2[7]
+                            if temp2[8]=="NWI":
+                                wind=""
+                            else:
+                                wind=temp2[8]
+                            temp2[4]=""
+                            temp2[5]=distance
+                            temp2[6]=""
+                            temp2[7]=meet
+                            temp2[8]=date
+                            temp2.append(wind)
+                            cols=temp2
                         else:
-                            temp2[7]=temp2[8]
-                            del temp2[8]
-                        cols=temp2
-
-                # finding events and genders
-                if l == 0:
-                    event = events[k].split()
-                    gender=event[-1]
-                    event = " ".join(event[0:len(event)-1])
-                    if gender == '(Men)':
-                        gender = 'Men'
-                    elif gender == '(Women)':
-                        gender = 'Women'
-                    l = 1
-
+                            temp2=cols
+                            distance = temp2[4]
+                            meet=temp2[6]
+                            date=temp2[7]
+                            temp2[4]=""
+                            temp2[5]=distance
+                            temp2[6]=""
+                            
+                            temp2[7]=meet
+                            temp2.append(date)
+                            temp2.append("")
+                            # print(len(temp2))
+                            # break
+                            # if len(cols)!= 9:
+                            #     temp2[7]=""
+                            # else:
+                            #     temp2[7]=temp2[8]
+                            #     del temp2[8]
+                            cols=temp2
+                            #print(event)
+                if event=="Heptathlon" or event=="Decathlon":
+                    points=cols[4]
+                    meet=cols[5]
+                    date=cols[6]
+                    cols[4]=""
+                    cols[5]=""
+                    cols[6]=points
+                    cols.append(meet)
+                    cols.append(date)
+                    cols.append("")
                 # adding NaN if there is nothing in wind column
-                if len(cols)==7:
-                    cols.append('NaN')
-                elif len(cols)==8 and cols[7]=='':
-                    cols[7]='NaN'
+                # if len(cols)==7:
+                #     cols.append('')
+                # elif len(cols)==8 and cols[7]=='':
+                #     cols[7]=''
                 cols.append(event)
                 cols.append(conference)
                 cols.append(gender)
+                # cols.append(k)
+                # if event == "10,000 Meters":
+                #     print(cols)
+                #     split=cols[4].split(":")
+                #     print(split)
+                #     cols[4]=":".join(split[0:2])
                 data.append(cols)
     with open(name, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
