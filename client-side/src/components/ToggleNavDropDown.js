2f97ccd7449c218857fd3533a1bd961f97217825
Gabe Vallejo <gabrielvallejo19@augustana.edu>
2023-05-10 10:58:33 -0500

    made logo

--- client-side/src/components/ToggleNavDropDown.js
+++ client-side/src/components/ToggleNavDropDown.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import burgerIcon from "../icons/burger-icon.png";
+import logo from "../icons/AALogo.png";
 
   const ToggleNavDropDown = ({ children }) => {
   const [showChildren, setShowChildren] = useState(false);
@@ -10,8 +10,7 @@ import burgerIcon from "../icons/burger-icon.png";
 
   return (
     <div className="navDropdown">
-
-      <button onClick={handleButtonClick}><img src="burgerIcon"></img></button>
+      <button onClick={handleButtonClick}><img className="navLogo" src={logo} alt="AALogo"/></button>
       {showChildren && children}
     </div>
   );
