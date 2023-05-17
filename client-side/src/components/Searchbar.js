eaca75df690ae41d96220cf4db011b8c37cd78d7
Jon <joncathcart19@augustana.edu>
2023-05-17 17:53:56 -0500

    queries for school profiles

--- client-side/src/components/Searchbar.js
+++ client-side/src/components/Searchbar.js
@@ -4,52 +4,61 @@ import DisplayHidden from "./DisplayHidden";
 
 //import SearchIcon from '@mui/icons-material/Search';
 
-export default function Searchbar(){
+const Searchbar = ({ onSearch }) => {
 
     const [trackList, setTrackList] = useState([]);
     const [search, setSearch] = useState('');
+    const [selectedValue, setSelectedValue] = useState('');
     useEffect(() => {
       Axios.get("http://localhost:3001/Searchbar").then((response) => {
         setTrackList(response.data);
         console.log(response.data);
       });
     }, []);
-
-    const handleInputChange = (event) => {
-        setSearch(event.target.value);
-      };
+      
+    const handleInputChange = (e) => {
+        setSearch(e.target.value);
+    };
+      
     const handleResult = (val) => {
-        console.log('Clicked ' + val.College);
-    }
-      return(
-
-        <div className="searchbar">
-            
-                <div className="searchInputs">
-                    <input type="text" placeholder="Search a College..." onChange={handleInputChange} />
-                    <div className ='searchIcon'>
-                        {/* <SearchIcon/> */}
-                    </div >
-                    <div className="searchbar-results">
-                        {search.length !== 0 && trackList.filter((val) => {
-                            return search === '' 
-                            ? val 
-                            : val.College.toLowerCase().includes(search.toLowerCase());
-                            }).map((val, index) => {
-                                return ( 
-                                    <a key={index} href={val.link} target="_blank" onClick={() => handleResult(val)}>
-                                        <p className='dataItem'>
-                                            {val.College}
-                                        </p>
-                                    </a>
-                                );
-                            })
-                        }
-                    </div>
-                </div>
+        setSelectedValue(val.College);
+        setSearch(val.College);
+        onSearch(val.College)
+    };
+      
+    return (
+      <div className="searchbar">
+        <div className="searchInputs">
+          <input
+            type="text"
+            placeholder="Search a College..."
+            value={search}
+            onChange={handleInputChange}
+          />
+          <div className="searchIcon">{/* <SearchIcon/> */}</div>
+          <div className="searchbar-results">
+            {search.length !== 0 &&
+              trackList
+                .filter((val) =>
+                  search === ''
+                    ? val
+                    : val.College.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((val, index) => {
+                  return (
+                    <a
+                      key={index}
+                      href={val.link}
+                      target="_blank"
+                      onClick={() => handleResult(val)}
+                    >
+                      <p className="dataItem">{val.College}</p>
+                    </a>
+                  );
+                })}
+          </div>
         </div>
-
-    )
-
-
-}
+      </div>
+    );
+  };
+export default Searchbar; 
